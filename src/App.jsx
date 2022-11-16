66200c28aed23de4bf15609fa28e9e0cbd9c88ce
SarathVavachan <sarathvavachan@gmail.com>
2022-11-16 12:20:11 +0400

    vite-react-minimal - 0.1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-slate-900 min-h-screen text-white p-2">
+      <h1 className="text-2xl text-center">Vite React Minimal</h1>
+    </div>
+  );
+}
+
+export default App;
